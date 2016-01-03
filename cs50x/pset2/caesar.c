edf0b48592a39fc79c51a2a1ad9a68b128b0d711
Nicholas Asimov <kolyaazimov@gmail.com>
2016-01-03 06:37:02 +0000

    Fixed compiled files being pushed

---
+++ cs50x/pset2/caesar.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <cs50.h>
+#include <string.h>
+#include <stdlib.h>
+
+int main(int argc, string argv[])
+{
+    if (argc == 2)
+    {
+        string p = GetString();
+        int k = atoi(argv[1]);
+        
+        
+    }
+    else
+    {
+        return 1;
+    }
+    
+    return 0;
+}
